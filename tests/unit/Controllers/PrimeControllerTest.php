bf140c6897430a16bc88916eecfa7c90ca58f426
boneff <boneff.i@gmail.com>
2018-03-13 22:57:42 +0200

    Initial commit

---
+++ tests/unit/Controllers/PrimeControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: boneff
+ * Date: 3/13/18
+ * Time: 10:52 PM
+ */
+
+namespace Prime\Test\Controllers;
+
+
+class PrimeControllerTest extends \PHPUnit_Framework_TestCase
+{
+
+}
---
+++ tests/unit/Validators/PrimeValidatorTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: boneff
+ * Date: 3/13/18
+ * Time: 10:53 PM
+ */
+
+namespace Prime\Test\Validators;
+
+
+class PrimeValidatorTest extends \PHPUnit_Framework_TestCase
+{
+
+}
